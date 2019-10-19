672679e4d947dd85b921d564002e80bbeabc3313
ldiaz94 <ldiaz9@outlook.com>
2019-10-19 20:07:40 +0100

    Add project files.

---
+++ Filterv1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Filterv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Image srcImage = Image.FromFile("res/mountains.jpg");
+                Image filteredImage;
+                Bitmap bmpImage = new Bitmap(srcImage);
+
+                Bitmap filteredBitmap = new Bitmap(bmpImage.Width, bmpImage.Height);
+                Color originalPixel;
+                Color newPixel;
+
+                for (int i = 0; i < bmpImage.Width; i++)
+                {
+                    for (int j = 0; j < bmpImage.Height; j++)
+                    {
+                        // remove the red colour from the image
+                        originalPixel = bmpImage.GetPixel(i, j);
+                        newPixel = Color.FromArgb(0, originalPixel.G, originalPixel.B);
+                        filteredBitmap.SetPixel(i, j, newPixel);
+                    }
+                }
+
+                filteredImage = (Image) filteredBitmap;
+                filteredImage.Save("res/mountains-nored.jpg", ImageFormat.Jpeg);
+                Console.WriteLine("All good");
+                Console.ReadKey();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
